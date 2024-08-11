923940dddfadb92b02f4b87e31884bfb805719f7
Rich <richardstrain@gmail.com>
2024-08-11 17:03:56 -0400

    Find Category By Id Working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,9 +15,16 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// find category by id with associatiated products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
