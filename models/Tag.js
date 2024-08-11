360bd1313a11a44aac2f430d510dd5dc0faab036
Rich <richardstrain@gmail.com>
2024-08-11 15:16:40 -0400

    Models Established - Seeds Working

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,10 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Defines the 3rd table to map the relationship
+  through: {
+    model: ProductTag,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Defines the 3rd table to map the relationship
+  through: {
+    model: ProductTag,
+  },
+});
 
 module.exports = {
   Product,
