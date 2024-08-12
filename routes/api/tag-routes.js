010caa3a831f2208e9fae49cec966eae6bff29d1
Rich <richardstrain@gmail.com>
2024-08-12 14:38:24 -0400

    Delete Tag By Id Working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,7 +37,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-/// Update Tag Record By Id
+/// Update Tag Record By Param Id
 router.put('/:id', async (req, res) => {
   try {
     const tagData = await Tag.update(req.body, { where: { id: req.params.id } });
@@ -47,8 +47,20 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// Destroy Tag Record By Param Id
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ alert: 'No Tag Found For That Id!' });
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
