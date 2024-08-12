ac7afa29dedf1d6417477bb99fe3d0ac747b9516
Rich <richardstrain@gmail.com>
2024-08-12 14:23:04 -0400

    Update Tag Working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,8 +37,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+/// Update Tag Record By Id
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, { where: { id: req.params.id } });
+    res.status(200).json(`Tag Id: ${req.params.id} has been updated`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
