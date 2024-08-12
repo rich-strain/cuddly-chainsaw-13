efccefa0da4d60011f2ba5dad787f519f265856c
Rich <richardstrain@gmail.com>
2024-08-12 17:01:27 -0400

    Added Error Management For No Record Found

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -9,6 +9,10 @@ router.get('/', async (req, res) => {
     const tagData = await Tag.findAll({
       include: [{ model: Product }],
     });
+    if (!tagData) {
+      res.status(404).json({ alert: 'No Tags Found!' });
+      return;
+    }
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -21,6 +25,10 @@ router.get('/:id', async (req, res) => {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
+    if (!tagData) {
+      res.status(404).json({ alert: 'No Tag Found For That Id!' });
+      return;
+    }
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -41,6 +49,10 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     const tagData = await Tag.update(req.body, { where: { id: req.params.id } });
+    if (!tagData) {
+      res.status(404).json({ alert: 'No Tag Found For That Id!' });
+      return;
+    }
     res.status(200).json(`Tag Id: ${req.params.id} has been updated`);
   } catch (err) {
     res.status(500).json(err);
@@ -56,6 +68,7 @@ router.delete('/:id', async (req, res) => {
 
     if (!tagData) {
       res.status(404).json({ alert: 'No Tag Found For That Id!' });
+      return;
     }
     res.status(200).json(`Tag Id: ${req.params.id} has been deleted!`);
   } catch (err) {
