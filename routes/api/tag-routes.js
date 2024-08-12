b98e8e1e37fd79a05f261e0a3368f1652c8af825
Rich <richardstrain@gmail.com>
2024-08-12 14:11:17 -0400

    Tag Creation Working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,8 +27,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// Create New Tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
